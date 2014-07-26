4afcb1032012f86df709e9cb6ac9fc4ad3abf95d
Mika Attila <radiantstatue@gmail.com>
2014-07-26 16:52:06 +0200

    FAST

--- tagc-rust/src/main.rs
+++ tagc-rust/src/main.rs
@@ -1,7 +1,6 @@
 mod tokenizer;
 
 fn main() {
-    use std::io::BufferedReader;
     use std::io::fs::File;
     let ref filename = std::os::args()[1];
     let mut file = File::open(&Path::new(filename.as_slice()));
