fbf6faed17ea93c110b2346bc23bb45f91145125
Mika Attila <radiantstatue@gmail.com>
2014-07-26 16:51:48 +0200

    Kiko

--- tagc-rust/src/main.rs
+++ tagc-rust/src/main.rs
@@ -4,7 +4,6 @@ fn main() {
     use std::io::BufferedReader;
     use std::io::fs::File;
     let ref filename = std::os::args()[1];
-    println!("COMPILING {}", filename);
     let mut file = File::open(&Path::new(filename.as_slice()));
     let string = file.read_to_string();
     println!("{}", tokenizer::tokenize(string.unwrap().as_slice()));
