92e5ff9811db498f33e09a39426fafbcba70fbd2
Mika Attila <radiantstatue@gmail.com>
2014-07-26 16:43:38 +0200

    Swag

--- tagc-rust/src/main.rs
+++ tagc-rust/src/main.rs
@@ -1,6 +1,13 @@
 mod tokenizer;
 
 fn main() {
+    use std::io::BufferedReader;
+    use std::io::fs::File;
     let ref filename = std::os::args()[1];
     println!("COMPILING {}", filename);
+    let file = File::open(&Path::new(filename.as_slice()));
+    let mut reader = BufferedReader::new(file);
+    for (lnum, line) in reader.lines().enumerate() {
+        print!("{} {}", lnum, line.unwrap());
+    }
 }
